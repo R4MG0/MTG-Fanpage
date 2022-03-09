35a46341f56715acfb1650ab172d7b3e22cf720f
Herren Marco <bherma@bbcag.ch>
2022-03-09 16:30:13 +0100

    added a lot of stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
-document.getElementById("link").addEventListener("click",  event => open("https://magic.wizards.com/en/articles/archive/magic-academy/anatomy-magic-card-2006-10-21"));
+// alert("This Website is just a Fanpage");
+
+document.getElementById("link").addEventListener("click",  event => open("https://magic.wizards.com/en/articles/archive/magic-academy/anatomy-magic-card-2006-10-21", "_blank"));
+document.getElementById("wikipedia").addEventListener("click",  event => open("https://en.wikipedia.org/wiki/Magic:_The_Gathering"));
+document.getElementById("commanderInformations").addEventListener("click",  event => open("https://magic.wizards.com/de/formats/commander"));
