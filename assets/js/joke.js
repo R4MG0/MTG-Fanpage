c3f7b518e8d33c436a25dfa841725c1e626814b6
Herren Marco <bherma@bbcag.ch>
2022-03-10 15:58:09 +0100

    everything works

---
+++ assets/js/joke.js
@@ -0,0 +1,10 @@
+
+async function getJoke () {
+    const response = await fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single');
+    const completejson = await response.json();
+    const joke = completejson.joke;
+    console.log(joke);
+    document.getElementById('joke').innerHTML = joke;
+}
+
+getJoke();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
-// alert("This Website is just a Fanpage");
+let bool = false;
 
-async function getJoke () {
-    const response = await fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single');
-    const completejson = await response.json();
-    const joke = completejson.joke;
-    console.log(joke);
-    document.getElementById('joke').innerHTML = joke;
+function setBool(value) {
+    bool = value;
 }
 
-getJoke();
+function getBool() {
+    return bool;
+}
+
+function isFalse() {
+    if(getBool() === false) {
+        alert("This Website is just a Fanpage");
+        setBool(true);
+    }
+    
+}
+
+
+isFalse();
